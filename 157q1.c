0b09e140337423044cf6907aa0cc2c35e3de0029
davdleet <46433051+davdleet@users.noreply.github.com>
2020-01-27 17:22:16 +0900

    Add files via upload

---
+++ 157q1.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void)
+{	
+	int num=0,num1=0,total=0;
+	while(num<5)
+	{
+		while(num1<1)
+		{
+			printf("number?: ");
+			scanf("%d", &num1);
+		}
+		total+=num1;
+		num+=1;
+		num1=0;
+	}
+	printf("total: %d", total);
+	return 0;
+} 
---
+++ 157q2.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{	
+	int count=0, count1=0;
+		while(count<5)
+		{
+			count1=0;
+			while(count>count1)
+			{
+				printf("o ");
+				count1++;
+			}
+		printf("*\n\n");
+		count++;
+		}
+} 
